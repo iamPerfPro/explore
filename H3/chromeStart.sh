4a83d5789045a636dd6bf2386f9667859fe43b14
Abhinav <abnv418@gmail.com>
2023-06-21 12:16:27 +0530

    Chrome Command for bringing up HTTP3 page.

---
+++ H3/chromeStart.sh
@@ -0,0 +1,6 @@
+/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome \
+--user-data-dir=/tmp/any/ \
+--ignore-certificate-errors-spki-list="" \
+--origin-to-force-quic-on=ipaddress:port \
+--incognito \
+https://ipaddress:port/
